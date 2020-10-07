803e69dee12f962634f041bb2d48b9deed059e9e
Ankith Jain <ankith.jain@mavenir.com>
2020-10-07 23:55:32 +0530

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '15a651447702aafa2253527b7e7db4d7'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd5fb6b55-f1be-4c34-a70d-e915e8d07181'
+};
