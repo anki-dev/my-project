44ec3ff0a2f8576005f481a7cfb9018e0d883754
Ankith Jain <ankith.jain@mavenir.com>
2020-10-08 00:29:29 +0530

    host condition added

--- config/database.js
+++ config/database.js
@@ -7,8 +7,8 @@ module.exports = ({
       connector: 'bookshelf',
       settings: {
         "client": "postgres",
-        "host": "${process.env.DATABASE_HOST}",
-        "port": "${process.env.DATABASE_PORT}",
+        "host": "${process.env.DATABASE_HOST || '0.0.0.0'}",
+        "port": "${process.env.DATABASE_PORT || 1337}",
         "database": "${process.env.DATABASE_NAME}",
         "username": "${process.env.DATABASE_USERNAME}",
         "password": "${process.env.DATABASE_PASSWORD}",
